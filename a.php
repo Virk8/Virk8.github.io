a9a6a1d5d0ab550b3c671a1e36b2f7b32e3574da
Virk <125840564+Virk8@users.noreply.github.com>
2024-07-13 01:46:18 -0300

    jh

---
+++ a.php
@@ -0,0 +1,38 @@
+<?php
+function gerasenha($tamanho = 8, $maiusculas = true, $numeros = true, $simbolos = false)
+{
+$lmin = 'abcdefghijklmnopqrstuvwxyz';
+$lmai = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+$num = '1234567890';
+$simb = '!@#$%*-';
+$retorno = '';
+$caracteres = '';
+
+$caracteres .= $lmin;
+if ($maiusculas) $caracteres .= $lmai;
+if ($numeros) $caracteres .= $num;
+if ($simbolos) $caracteres .= $simb;
+
+$len = strlen($caracteres);
+for ($n = 1; $n <= $tamanho; $n++) {
+$rand = mt_rand(1, $len);
+$retorno .= $caracteres[$rand-1];
+}
+return $retorno;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+        word-break: break-all;}
+    </style>
+</head>
+<body>
+    <p><?php echo htmlspecialchars(gerasenha(10000)); ?></p>
+</body>
+</html>
